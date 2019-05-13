4fb24e88f8fe141f9974a34f07a1a3b2a51718be
Niklas <niklasld@gmail.com>
2019-05-13 15:14:01 +0200

    updated test

--- src/test/java/com/example/helloworld/HomeControllerTest.java
+++ src/test/java/com/example/helloworld/HomeControllerTest.java
@@ -1,5 +1,16 @@
+package com.example.helloworld;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HomeControllerTest {
 
+    //her tester vi udenfor Spring context
+    private HomeController homeController = new HomeController();
+
+    @Test
+    public void home() {
+        assertEquals("index", homeController.home());
+    }
 }
